3bdc2c19da9e30106879296796a1b6c47ea48732
User <User@WS-SENDERUK>
2023-08-01 12:53:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = new int[] { 1, 3, 5, 7, 11, 13, 17 };
+
+            Console.WriteLine("Квадраты чисел");
+            Console.WriteLine();
+            Console.WriteLine("Дан ряд чисел:");
+
+            for (int a = 0; a < numbers.Length; a++)
+            {
+                Console.Write(numbers[a] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Квадраты этих чисел:");
+
+            for (int a = 0; a < numbers.Length; a++)
+            {
+                Console.Write(numbers[a] * numbers[a] + " ");
+            }
+        }
+    }
+}
